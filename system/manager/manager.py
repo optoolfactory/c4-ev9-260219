ed979014b4f8002d874e0b7865f63f1dcb402150
optoolfactory <80941254+optoolfactory@users.noreply.github.com>
2026-03-30 21:03:25 +0900

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -76,16 +76,16 @@ def manager_init() -> None:
   params.put_bool("IsReleaseSpBranch", build_metadata.release_sp_channel)
   params.put("HardwareSerial", serial)
 
-  ev9_platform_bundle = {
-    "platform": "KIA_EV9",
-    "make": "Kia",
-    "brand": "hyundai",
-    "model": "EV9",
-    "year": ["2025", "2026"],
-    "package": "Smart Cruise Control (SCC)",
-    "name": "Kia EV9 2025-26",
-  }
-  params.put("CarPlatformBundle", ev9_platform_bundle)
+  #ev9_platform_bundle = {
+    #"platform": "KIA_EV9",
+    #"make": "Kia",
+    #"brand": "hyundai",
+    #"model": "EV9",
+    #"year": ["2025", "2026"],
+    #"package": "Smart Cruise Control (SCC)",
+    #"name": "Kia EV9 2025-26",
+  #}
+  #params.put("CarPlatformBundle", ev9_platform_bundle)
 
   # set dongle id
   reg_res = register(show_spinner=True)
